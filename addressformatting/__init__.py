100ba59b7c9ac11ac621a5cbc0c179b90d959dab
Friedrich Lindenberg <friedrich@pudo.org>
2021-06-25 15:58:30 +0200

    Refactor rendering a bit

--- addressformatting/__init__.py
+++ addressformatting/__init__.py
@@ -1,1 +1,4 @@
-from .format import AddressFormatter
+from addressformatting.format import AddressFormatter
+
+__version__ = "1.0.2"
+__all__ = ["AddressFormatter"]
--- addressformatting/format.py
+++ addressformatting/format.py
@@ -1,41 +1,23 @@
-import yaml
-import pystache
 import os
+import yaml
 
+from addressformatting.util import clean_address, first, render
 
-def first(address):
-    def _first(content):
-        tokens = [token.strip() for token in content.split("||")]
-        for t in tokens:
-            result = pystache.render(t, address)
-            if result.strip() != "":
-                return result
-        return ""
-
-    return _first
+PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_FILE = os.path.join(PACKAGE_DIR, "data/worldwide.yml")
+TEMPLATE_FILE = os.path.abspath(TEMPLATE_FILE)
 
 
 class AddressFormatter:
-    def __init__(self, config=None):
-        # if no opencage data file is specified in the configuration
-        # we fall back to the one included with this package
-        if config is None:
-            # if not found, use a relative path definition
-            my_dir = os.path.dirname(os.path.abspath(__file__))
-            config = os.path.abspath(os.path.join(my_dir, "data/worldwide.yml"))
-
-        with open(config, "r", encoding="utf-8") as fp:
+    def __init__(self):
+        with open(TEMPLATE_FILE, "r", encoding="utf-8") as fp:
             self.model = yaml.load(fp, Loader=yaml.FullLoader)
 
-    def format(self, address, country=None):
+    def _format(self, address, country=None):
         search_key = country.upper() if country is not None else "default"
         fmt = self.model.get(search_key, None)
         if fmt is None:
             fmt = self.model.get("default", None)
-        if fmt is None:
-            raise RuntimeError(
-                "Configuration file for address formatter has no default value!"
-            )
 
         # Some country configurations redirect to other countries but
         # change the country name in the process:
@@ -44,7 +26,7 @@ class AddressFormatter:
             country = fmt.get("change_country")
             if country is not None:
                 address["country"] = country
-            return self.format(address, country=use_country)
+            return self._format(address, country=use_country)
 
         cleaned_address = {}
         for key, value in address.items():
@@ -52,7 +34,11 @@ class AddressFormatter:
                 cleaned_address[key] = value
 
         cleaned_address["first"] = first(cleaned_address)
-        return pystache.render(fmt["address_template"], cleaned_address).strip()
+        return render(fmt["address_template"], cleaned_address)
+
+    def format(self, address, country=None):
+        return clean_address(self._format(address, country=country))
 
     def one_line(self, address, country=None):
-        return ", ".join(self.format(address, country=country).split("\n"))
+        line = ", ".join(self.format(address, country=country).split("\n"))
+        return clean_address(line)
