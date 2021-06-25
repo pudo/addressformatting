d0050b66f5a6773b6f5c67aa9f842e3f91950c32
Friedrich Lindenberg <friedrich@pudo.org>
2021-06-25 15:36:16 +0200

    Try to run tests in CI

--- tests/test_germany.py
+++ tests/test_germany.py
@@ -1,21 +1,25 @@
 import unittest
-from international_address_formatter import AddressFormatter
+from addressformatting import AddressFormatter
+
 
 class TestAddressFormatterGermany(unittest.TestCase):
-
     def test_street(self):
         formatter = AddressFormatter()
         self.assertEqual(
             "Bahnhofstr. 10, 86150 Augsburg, Germany",
-            formatter.one_line({
-                'road': "Bahnhofstr.",
-                'house_number': "10",
-                'postcode': "86150",
-                'city': "Augsburg",
-                'state': "Bayern",
-                'country': "Germany"
-            }, country='DE')
+            formatter.one_line(
+                {
+                    "road": "Bahnhofstr.",
+                    "house_number": "10",
+                    "postcode": "86150",
+                    "city": "Augsburg",
+                    "state": "Bayern",
+                    "country": "Germany",
+                },
+                country="DE",
+            ),
         )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
