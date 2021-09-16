b50c4af5948c34b5fefca99aba0d893c22903c6a
Friedrich Lindenberg <friedrich@pudo.org>
2021-09-16 18:36:28 +0200

    Bump version: 1.1.2 → 1.2.0

--- addressformatting/__init__.py
+++ addressformatting/__init__.py
@@ -1,4 +1,4 @@
 from addressformatting.format import AddressFormatter
 
-__version__ = "1.1.2"
+__version__ = "1.2.0"
 __all__ = ["AddressFormatter"]
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md") as f:
 
 setup(
     name="addressformatting",
-    version="1.1.2",
+    version="1.2.0",
     description="Formatting utility for international postal addresses",
     long_description=long_description,
     long_description_content_type="text/markdown",
